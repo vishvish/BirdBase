987abdc35a34a9d53fe20fbc12de459b6e103e34
Vish Vishvanath <vish.vishvanath@gmail.com>
2011-05-10 12:49:34 +0100

    Bumping up version number in Rakefile.

--- rakefile.rb
+++ rakefile.rb
@@ -4,14 +4,14 @@ require 'bundler/setup'
 require 'rake/clean'
 require 'flashsdk'
 
-version = "0.5.1"
+version = "0.7.5"
 
 def configure t
   t.library_path << 'lib'
   t.source_path << 'src'
   t.include_sources << 'src'
   t.optimize = true
-  t.target_player = '10.0'
+  t.target_player = '10.1'
 end
 
 compc "bin/birdbase-#{version}.swc" do |t|
